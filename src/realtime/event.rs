e5e1b87291676bbdb47906e739c9e93bf276d67b
brakenium <brakenium@example.com>
2022-11-03 20:55:54 +0100

    Cleanup and deserialize CharacterID's correctly

--- src/realtime/event.rs
+++ src/realtime/event.rs
@@ -37,9 +37,7 @@ impl serde::Serialize for EventNames {
         use self::EventNames::*;
 
         match *self {
-            AchievementEarned => {
-                serializer.serialize_unit_variant("EventNames", 0, "AchievementEarned")
-            }
+            AchievementEarned => serializer.serialize_unit_variant("EventNames", 0, "AchievementEarned"),
             BattleRankUp => serializer.serialize_unit_variant("EventNames", 1, "BattleRankUp"),
             Death => serializer.serialize_unit_variant("EventNames", 2, "Death"),
             ItemAdded => serializer.serialize_unit_variant("EventNames", 3, "ItemAdded"),
@@ -51,19 +49,11 @@ impl serde::Serialize for EventNames {
 
                 serializer.serialize_str(&event_name)
             }
-            PlayerFacilityCapture => {
-                serializer.serialize_unit_variant("EventNames", 8, "PlayerFacilityCapture")
-            }
-            PlayerFacilityDefend => {
-                serializer.serialize_unit_variant("EventNames", 9, "PlayerFacilityDefend")
-            }
+            PlayerFacilityCapture => serializer.serialize_unit_variant("EventNames", 8, "PlayerFacilityCapture"),
+            PlayerFacilityDefend => serializer.serialize_unit_variant("EventNames", 9, "PlayerFacilityDefend"),
             ContinentLock => serializer.serialize_unit_variant("EventNames", 10, "ContinentLock"),
-            ContinentUnlock => {
-                serializer.serialize_unit_variant("EventNames", 11, "ContinentUnlock")
-            }
-            FacilityControl => {
-                serializer.serialize_unit_variant("EventNames", 12, "FacilityControl")
-            }
+            ContinentUnlock => serializer.serialize_unit_variant("EventNames", 11, "ContinentUnlock"),
+            FacilityControl => serializer.serialize_unit_variant("EventNames", 12, "FacilityControl"),
             MetagameEvent => serializer.serialize_unit_variant("EventNames", 13, "MetagameEvent"),
             PlayerLogin => serializer.serialize_unit_variant("EventNames", 14, "PlayerLogin"),
             PlayerLogout => serializer.serialize_unit_variant("EventNames", 15, "PlayerLogout"),
@@ -145,6 +135,7 @@ impl Display for Event {
 
 #[derive(Serialize, Deserialize, PartialEq, Debug, Clone)]
 pub struct PlayerLogin {
+    #[serde(deserialize_with = "deserialize_from_str")]
     pub character_id: CharacterID,
     #[serde(
         deserialize_with = "TimestampSeconds::<String>::deserialize_as",
@@ -157,6 +148,7 @@ pub struct PlayerLogin {
 
 #[derive(Serialize, Deserialize, PartialEq, Debug, Clone)]
 pub struct PlayerLogout {
+    #[serde(deserialize_with = "deserialize_from_str")]
     pub character_id: CharacterID,
     #[serde(
         deserialize_with = "TimestampSeconds::<String>::deserialize_as",
@@ -169,6 +161,7 @@ pub struct PlayerLogout {
 
 #[derive(Serialize, Deserialize, PartialEq, Debug, Clone)]
 pub struct Death {
+    #[serde(deserialize_with = "deserialize_from_str")]
     pub attacker_character_id: CharacterID,
     #[serde(deserialize_with = "deserialize_from_str")]
     pub attacker_fire_mode_id: FiremodeID,
@@ -178,6 +171,7 @@ pub struct Death {
     pub attacker_vehicle_id: VehicleID,
     #[serde(deserialize_with = "deserialize_from_str")]
     pub attacker_weapon_id: WeaponID,
+    #[serde(deserialize_with = "deserialize_from_str")]
     pub character_id: CharacterID,
     #[serde(deserialize_with = "deserialize_from_str")]
     pub character_loadout_id: Loadout,
@@ -198,6 +192,7 @@ pub struct Death {
 
 #[derive(Serialize, Deserialize, PartialEq, Debug, Clone)]
 pub struct VehicleDestroy {
+    #[serde(deserialize_with = "deserialize_from_str")]
     pub attacker_character_id: CharacterID,
     #[serde(deserialize_with = "deserialize_from_str")]
     pub attacker_loadout_id: Loadout,
@@ -205,6 +200,7 @@ pub struct VehicleDestroy {
     pub attacker_vehicle_id: VehicleID,
     #[serde(deserialize_with = "deserialize_from_str")]
     pub attacker_weapon_id: WeaponID,
+    #[serde(deserialize_with = "deserialize_from_str")]
     pub character_id: CharacterID,
     #[serde(deserialize_with = "deserialize_from_str")]
     pub facility_id: FacilityID,
@@ -225,11 +221,13 @@ pub struct VehicleDestroy {
 
 #[derive(Serialize, Deserialize, PartialEq, Debug, Clone)]
 pub struct GainExperience {
+    #[serde(deserialize_with = "deserialize_from_str")]
     pub character_id: CharacterID,
     #[serde(deserialize_with = "deserialize_from_str")]
     pub experience_id: ExperienceID,
     #[serde(deserialize_with = "deserialize_from_str")]
     pub loadout_id: Loadout,
+    #[serde(deserialize_with = "deserialize_from_str")]
     pub other_id: CharacterID,
     #[serde(
         deserialize_with = "TimestampSeconds::<String>::deserialize_as",
@@ -240,11 +238,13 @@ pub struct GainExperience {
     pub world_id: WorldID,
     #[serde(deserialize_with = "deserialize_from_str")]
     pub zone_id: ZoneID,
+    #[serde(deserialize_with = "deserialize_from_str")]
     pub amount: u16
 }
 
 #[derive(Serialize, Deserialize, PartialEq, Debug, Clone)]
 pub struct PlayerFacilityCapture {
+    #[serde(deserialize_with = "deserialize_from_str")]
     pub character_id: CharacterID,
     #[serde(deserialize_with = "deserialize_from_str")]
     pub facility_id: FacilityID,
@@ -262,6 +262,7 @@ pub struct PlayerFacilityCapture {
 
 #[derive(Serialize, Deserialize, PartialEq, Debug, Clone)]
 pub struct PlayerFacilityDefend {
+    #[serde(deserialize_with = "deserialize_from_str")]
     pub character_id: CharacterID,
     #[serde(deserialize_with = "deserialize_from_str")]
     pub facility_id: FacilityID,
